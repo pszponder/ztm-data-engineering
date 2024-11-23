dbd1b9f76b067c9f005322e17b25ab301a793208
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2024-11-23 22:58:27 +0000

    Exercises 1 for section 2

--- data-processing-with-spark/data/download_data.sh
+++ data-processing-with-spark/data/download_data.sh
@@ -1,1 +1,10 @@
-# TODO
+#!/bin/bash
+
+if [ -f "$0" ]; then script=$0; else script=$(command -v -- "$0"); fi
+dir=$(dirname -- "$script")
+
+echo "Writing data to $dir"
+
+wget -O "$dir/reviews.csv.gz" https://data.insideairbnb.com/united-kingdom/england/london/2024-09-06/data/reviews.csv.gz
+wget -O "$dir/calendar.csv.gz" https://data.insideairbnb.com/united-kingdom/england/london/2024-09-06/data/calendar.csv.gz
+wget -O "$dir/listings.csv.gz" https://data.insideairbnb.com/united-kingdom/england/london/2024-09-06/data/listings.csv.gz
