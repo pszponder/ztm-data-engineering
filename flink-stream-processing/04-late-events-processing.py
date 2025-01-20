4aaab739b4513470348eea2b4688a4feb15509b6
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2025-01-20 23:36:47 +0000

    Adds the next Flink demo

---
+++ flink-stream-processing/04-late-events-processing.py
@@ -0,0 +1,105 @@
+import json
+from dataclasses import dataclass
+from datetime import datetime
+
+from pyflink.common import Time
+from pyflink.common.serialization import SimpleStringSchema
+from pyflink.common.watermark_strategy import WatermarkStrategy, TimestampAssigner
+from pyflink.common.time import Duration
+from pyflink.common.typeinfo import Types
+from pyflink.datastream import OutputTag, StreamExecutionEnvironment, ProcessWindowFunction, TimeCharacteristic
+from pyflink.datastream.connectors.kafka import KafkaSource
+from pyflink.datastream.execution_mode import RuntimeExecutionMode
+from pyflink.datastream.window import TumblingEventTimeWindows
+
+@dataclass
+class Order:
+    order_id: str
+    customer_id: str
+    product_id: str
+    quantity: int
+    price: float
+    order_time: str
+
+
+def parse_order(json_str: str) -> Order:
+    data = json.loads(json_str)
+    order_time_seconds = datetime.fromisoformat(data["order_time"])
+    print("Order time: " + str(order_time_seconds))
+    return Order(
+        order_id=data.get("order_id", "unknown"),
+        customer_id=data.get("customer_id", "unknown"),
+        product_id=data.get("product_id", "unknown"),
+        quantity=data.get("quantity", 0),
+        price=float(data.get("price", 0.0)),
+        order_time=data.get("order_time", "unknown")
+    )
+
+
+class OrderTimestampAssigner(TimestampAssigner):
+    def extract_timestamp(self, value: Order, record_timestamp: int) -> int:
+        dt = datetime.fromisoformat(value.order_time)
+        return int(dt.timestamp() * 1000)
+
+
+class MyProcessWindowFunction(ProcessWindowFunction):
+    def process(self, key, context, elements):
+        total_quantity = 0
+        total_spent = 0
+        for order in elements:
+            total_quantity += order.quantity
+            total_spent += order.price
+
+        result = {
+            "user_id": key,
+            "total_quantity": total_quantity,
+            "total_spent": total_spent,
+            "window_start": context.window().start,
+            "window_end": context.window().end
+        }
+        return [json.dumps(result)]
+
+
+def main():
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_runtime_mode(RuntimeExecutionMode.STREAMING)
+    # Use event-time
+    env.set_stream_time_characteristic(TimeCharacteristic.EventTime)
+
+    kafka_source = KafkaSource.builder() \
+        .set_bootstrap_servers("localhost:9092") \
+        .set_topics("late-orders") \
+        .set_group_id("eventtime-demo") \
+        .set_value_only_deserializer(SimpleStringSchema()) \
+        .build()
+
+    watermark_strategy = WatermarkStrategy \
+        .for_bounded_out_of_orderness(Duration.of_seconds(10)) \
+        .with_timestamp_assigner(OrderTimestampAssigner())
+
+    stream = env.from_source(
+        kafka_source,
+        watermark_strategy=WatermarkStrategy.no_watermarks(),
+        source_name="kafka_source"
+    )
+
+    late_tag = OutputTag("late-events", Types.PICKLED_BYTE_ARRAY())
+
+    windowed_stream = stream \
+        .map(parse_order) \
+        .assign_timestamps_and_watermarks(watermark_strategy) \
+        .key_by(lambda x: x.customer_id) \
+        .window(TumblingEventTimeWindows.of(Time.seconds(30))) \
+        .side_output_late_data(late_tag) \
+        .process(MyProcessWindowFunction(), Types.STRING())
+
+    windowed_stream.print("Aggregated")
+
+    late_stream = windowed_stream.get_side_output(late_tag)
+    late_stream.print("LateEvents")
+
+    env.execute("Advanced Event-Time Window Demo")
+
+
+if __name__ == "__main__":
+    main()
---
+++ flink-stream-processing/late-events-producer.py
@@ -0,0 +1,75 @@
+import json
+import random
+import textwrap
+import time
+from datetime import datetime, timedelta
+
+from confluent_kafka import Producer
+
+
+def generate_order():
+    order_id = f"order-{random.randint(1000, 9999)}"
+    customer_id = f"customer-{random.randint(1, 10)}"
+    product_id = f"product-{random.randint(1, 200)}"
+    quantity = random.randint(1, 5)
+    price = round(random.uniform(5.0, 100.0), 2)
+    current_time = datetime.now()
+    # Sometimes intentionally late by up to 2 minutes
+    if random.random() < 0.2:
+        late_by = random.randint(60, 120)
+        # late_by = 0
+        event_time = current_time - timedelta(seconds=late_by)
+    else:
+        event_time = current_time
+
+    order_event = {
+        "order_id": order_id,
+        "customer_id": customer_id,
+        "product_id": product_id,
+        "quantity": quantity,
+        "price": price,
+        "order_time": event_time.isoformat()
+    }
+    return order_event
+
+
+def main():
+
+    config = {
+        "bootstrap.servers": "localhost:9092"
+    }
+
+    producer = Producer(config)
+
+    topic = "late-orders"
+
+    def delivery_callback(err, msg):
+        if err:
+            print("ERROR: Message failed delivery: {}".format(err))
+        else:
+            print(
+                textwrap.dedent(
+                f"""
+                    Produced event to topic {msg.topic()}:
+                    key = {msg.key().decode('utf-8')}
+                    value = {msg.value().decode('utf-8')}
+                """)
+            )
+
+    while True:
+        order = generate_order()
+        print(f"Sending order: {order}")
+
+        producer.produce(
+            topic,
+            key=str(order["customer_id"]),
+            value=json.dumps(order),
+            callback=delivery_callback,
+        )
+
+        producer.poll(0)
+
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
